28e1e621eab530c6d0ce1c8d728d1a6cb51f315d
Ganesan Chinnathambi <ganesancp@gmail.com>
2025-11-07 15:26:21 -0800

    minor updates

--- pages/login.py
+++ pages/login.py
@@ -8,16 +8,6 @@ from dash_iconify import DashIconify
 
 
 dash.register_page(__name__)
-loginButtonStyle =   {
-    "background": "#E418C2ff",
-    "padding": "5px 20px" ,
-    "border": "none",
-    "borderRadius": "20px",
-    "color": "white",
-    "fontSize":"16px",
-    "width":"100%"
-    
-  }
 
 loginWithGoogleStyle =   {
     "textDecoration": "white",
@@ -34,32 +24,7 @@ layout = dmc.Center(
                 style = {"width":'300px'},
                 method='POST',
                 children = [
-                    dmc.Text("Sign in ",  size='xl', fw=700),
-                    dmc.Text("Please log in to continue", c='gray', size='xs', mb = 10),
-                    dmc.TextInput(
-                        label="Email",
-                        name='email',
-                        placeholder="Enter your Email",
-                        required = True,
-
-                        leftSection=iconify(icon="ic:round-alternate-email", width=20),
-                    ),
-                    dmc.PasswordInput(
-                        mb=20,
-                        label="Password",
-                        placeholder="Enter your password",
-                        leftSection=iconify(icon="bi:shield-lock", width=20),
-                        name='password',
-                        required = True
-                    ),
-                    html.Button(
-                        children="Sign in", 
-                        n_clicks=0, 
-                        type="submit", 
-                        id="login-button", 
-                        style =loginButtonStyle
-                    ),
-                    dmc.Divider(label="Or continue with", mb = 10, mt = 10),
+                    dmc.Divider(label="Sign-in with", mb = 10, mt = 10, size="md"),
                     html.A(
                         href='/signingoogle', 
                         style = loginWithGoogleStyle,
@@ -74,14 +39,6 @@ layout = dmc.Center(
                             ),
                         ]
                     ),
-                    dmc.Flex(
-                         mt = 10,
-                        align = 'center',
-                        children = [
-                            dmc.Text(f" Don't have and Account?", c='gray', size = 'xs'),
-                            html.A('Sign up', href='/register', style = {'fontSize':'12px'})
-                        ]
-                    )  
                 ]
             )
         ]
